c0651bfbb25f47e785fbc37db8729749d80d2305
KBomba <KBomba@users.noreply.github.com>
2014-05-19 00:40:34 +0200

    Oops, forgot to add p= & d= :)

--- nicehash_test.bat
+++ nicehash_test.bat
@@ -2,5 +2,5 @@ set user=1KYMNgy5Lk9T8gJhqKQ86UEdmgPzcRpjvT
 set price=2.5
 set diff=0.01
 
-ccminer.exe -a x11 -o stratum+tcp://stratum.nicehash.com:3336 -u %user% -p %price%;%diff% -r 0 -R 1
+ccminer.exe -a x11 -o stratum+tcp://stratum.nicehash.com:3336 -u %user% -p p=%price%;d=%diff% -r 0 -R 1
 exit
