1239f8ff3d14dd408e3feb321cefe70b0008e4ea
zelante <djazot@gmail.com>
2014-05-19 02:02:33 +0300

    Update nicehash.bat
    add time to output in console

--- nicehash.bat
+++ nicehash.bat
@@ -2,6 +2,21 @@
 set nicehashs=false
 set failovers=false
 :loop
+set hour=%time:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set min=%time:~3,2%
+if "%min:~0,1%" == " " set min=0%min:~1,1%
+set secs=%time:~6,2%
+if "%secs:~0,1%" == " " set secs=0%secs:~1,1%
+
+set year=%date:~-4%
+set month=%date:~3,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+
+set datetimef=%year%-%month%-%day% %hour%:%min%:%secs%
+
 if "%nicehashs%"=="false" (
 	start "nicehash_test" /MIN nicehash_test.bat
 )
@@ -12,11 +27,11 @@ tasklist /FI "WINDOWTITLE eq nicehash_test*" 2>NUL | find /I /N "cmd.exe">NUL
 if "%ERRORLEVEL%"=="0" (
 	set nicehashs=true
 	set failovers=false
-	echo Price is good. Mining on Nicehash!
+	echo [%datetimef%] Price is good. Mining on Nicehash!
 	taskkill /F /T /FI "WINDOWTITLE eq nicehash_failover*" >NUL
 ) else (
 	set nicehashs=false
-	echo Nicehash checked. Price BTC/GH/Day is lower then we set...
+	echo [%datetimef%] Nicehash checked. Price BTC/GH/Day is lower then we set.
 )
 
 if "%nicehashs%"=="false" if "%failovers%"=="false" (
