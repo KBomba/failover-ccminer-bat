dd16aaa01b8afcef31d94fd73a6435275d9761ab
KBomba <KBomba@users.noreply.github.com>
2014-05-19 00:35:03 +0200

    Simplifying it a bit ;)

--- nicehash_failover.bat
+++ nicehash_failover.bat
@@ -1,4 +1,6 @@
-@echo off
-:: set failover pool
-ccminer.exe -q -a x11 -o stratum+tcp://europe1.darkcoin.miningpoolhub.com:20465 -u user.worker -p password
+set failover=stratum+tcp://useast1.cryptoalts.com:5555
+set user=BJcD6wg4nFZ5TLADaXk6mfJZovgGuiYnHF
+set pass=x
+
+ccminer.exe -a x11 -o %failover% -u %user% -p %pass%
 exit
--- nicehash_test.bat
+++ nicehash_test.bat
@@ -1,5 +1,6 @@
-@echo off
-:: p=2.60 - set minimum price for mining on nicehash.
-:: d=0.01 - set difficult for mining on nicehash
-ccminer.exe -q -a x11 -o stratum+tcp://stratum.nicehash.com:3336 -u BTC_Wallet -p p=2.60;d=0.01 -r 0 -R 1
+set user=1KYMNgy5Lk9T8gJhqKQ86UEdmgPzcRpjvT
+set price=2.5
+set diff=0.01
+
+ccminer.exe -a x11 -o stratum+tcp://stratum.nicehash.com:3336 -u %user% -p %price%;%diff% -r 0 -R 1
 exit
